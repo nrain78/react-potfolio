5dd30d7e32e518274bf5a0b6f90507f12cf7f704
nrain78 <noorrafique7890>
2025-03-02 09:56:49 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import About from './components/about/about'
-import Home from './components/Home/home'
+import Home from './components/home/home'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Projects from './components/projects/projects'
 import Contact from './components/contact/contact'
@@ -15,7 +15,7 @@ function App() {
       <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact/>} />
