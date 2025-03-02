5688bbce68479adffc0cd3a3eab542f339b8b086
nrain78 <noorrafique7890>
2025-03-02 10:59:32 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Navbar from './components/Navbar/Navbar'
+import Navbar from './components/navbar/Navbar'
 import About from './components/about/About'
 import Home from './components/home/Home'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
